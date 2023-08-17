1725afe7c6c0e5634683cb6b9baa3b91fe6d3275
aaron <aaronlj@hotmail.com>
2023-08-17 18:09:12 +0000

    Test

--- src/main/java/com/levelup/forestsandmonsters/Character.java
+++ src/main/java/com/levelup/forestsandmonsters/Character.java
@@ -1,5 +1,27 @@
 package com.levelup.forestsandmonsters;
 
 public class Character {
+
+    Static String DEFAULT_Name();
+    String name();
+
+    public Character(String name) {
+        this.name=name;
+        
+    }
+    public Character() {
+        
+
+public String getname(){
+return name();
+}
+public void enterMap(GameMap map){
+
+}
+  public Position getposition(){
+    return Position();
+  }  
+  public void move(DIRECTION direction){
     
+  }
 }
