3d771516d40372ac6e088df8eb7c3c527fbaba14
root <gvclavanya1@yahoo.com>
2023-08-17 20:31:28 +0000

    t3

--- src/main/java/com/levelup/forestsandmonsters/Character.java
+++ src/main/java/com/levelup/forestsandmonsters/Character.java
@@ -1,10 +1,13 @@
 package com.levelup.forestsandmonsters;
 
+import com.levelup.forestsandmonsters.GameController.DIRECTION;
+
 public class Character {
 
   static final String DEFAULT_NAME = "Character";
   String name;
   Position position;
+  GameMap map;
 
   public Character(String name) {
     this.name = name;
@@ -19,11 +22,16 @@ public class Character {
   }
 
   public void enterMap(GameMap map) {
-
+    this.map = map;
+    map.calculatePosition(null, null);
   }
 
   public Position getPosition() {
     return position;
   }
 
+  public void move(DIRECTION direction) {
+    position = map.calculatePosition(position, direction);
+
+  }
 }
