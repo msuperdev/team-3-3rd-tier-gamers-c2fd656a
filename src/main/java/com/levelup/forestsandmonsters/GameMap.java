7fcc18dda6b401df2ea0b4debde0659722f2681b
msuperdev <msuperdev@gmail.com>
2023-08-17 17:40:50 +0000

    Updated calculatePosition in GameMap (Graham)

--- src/main/java/com/levelup/forestsandmonsters/GameMap.java
+++ src/main/java/com/levelup/forestsandmonsters/GameMap.java
@@ -1,5 +1,7 @@
 package com.levelup.forestsandmonsters;
 
+import com.levelup.forestsandmonsters.GameController.DIRECTION;
+
 public class GameMap {
     int numPositions;
 
@@ -11,7 +13,7 @@ public class GameMap {
         return numPositions;
     }
 
-    public Position calculatePosition(Position startingPosition, String direction){
+    public Position calculatePosition(Position startingPosition, DIRECTION direction){
         return startingPosition;
     }
 
