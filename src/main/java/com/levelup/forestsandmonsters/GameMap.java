79d7948db1285e35dc71635d145434a09f39f70f
msuperdev <msuperdev@gmail.com>
2023-08-17 17:59:07 +0000

    Updated calculatePosition (Graham)

--- src/main/java/com/levelup/forestsandmonsters/GameMap.java
+++ src/main/java/com/levelup/forestsandmonsters/GameMap.java
@@ -1,6 +1,7 @@
 package com.levelup.forestsandmonsters;
 
 import com.levelup.forestsandmonsters.GameController.DIRECTION;
+import static com.levelup.forestsandmonsters.GameController.DIRECTION.*;
 
 public class GameMap {
     int numPositions;
@@ -14,7 +15,25 @@ public class GameMap {
     }
 
     public Position calculatePosition(Position startingPosition, DIRECTION direction){
-        return startingPosition;
+        Position newPosition = new Position(startingPosition.getX(),startingPosition.getY());
+
+        if(direction == NORTH) {
+            newPosition.y++;
+
+        } else if(direction == SOUTH) {
+            newPosition.y--;
+
+        } else if(direction == EAST) {
+            newPosition.x++;
+
+        } else if(direction == WEST) {
+            newPosition.x--;
+        } 
+
+        if (isPositionValid(newPosition) == true){
+            return newPosition;
+        } else return startingPosition;
+
     }
 
     public boolean isPositionValid(Position p){
