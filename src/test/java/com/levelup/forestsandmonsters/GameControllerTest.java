9a7bf183c8b08ace24409ae9317fb494b9935707
msuperdev <msuperdev@gmail.com>
2023-08-18 14:06:19 +0000

    cleaner game controller tests

--- src/test/java/com/levelup/forestsandmonsters/GameControllerTest.java
+++ src/test/java/com/levelup/forestsandmonsters/GameControllerTest.java
@@ -3,22 +3,10 @@ package com.levelup.forestsandmonsters;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-
 import java.awt.Point;
-
 import org.junit.Test;
 
-import com.levelup.forestsandmonsters.GameController.DIRECTION;
-
 public class GameControllerTest {
-    private static final DIRECTION NORTH = null;
-
-    //private static enum DIRECTION {
-    //    NORTH, SOUTH, EAST, WEST
-   // }
-
-    
 
     @Test
     public void initializationCreatesResults() {
@@ -26,36 +14,29 @@ public class GameControllerTest {
         assertNotNull(testObj.status);
     }
 
-   @Test
-    public void CheckGameControllerStatusResults() {
+    @Test
+    public void createCharacter() {
         GameController testObj = new GameController();
         testObj.createCharacter("Purna");
-        assertEquals("Purna",testObj.status.characterName);
+        assertEquals("Purna", testObj.status.characterName);
     }
 
- @Test
-    public void CheckstartGame() {
+    @Test
+    public void startGame() {
         GameController testObj = new GameController();
         testObj.startGame();
-        //assertEquals(100, testObj.getTotalPositions(), 100 - testObj.getTotalPositions());
-        System.out.println("Positions " + testObj.getTotalPositions());
-              assertEquals("Should be 100", 100, testObj.getTotalPositions());
-               assertNotEquals("Should be 100", 10, testObj.getTotalPositions());
-//        assertNotNull(testObj.status);
-
+        assertEquals("Should be 100", 100, testObj.getTotalPositions());
+        assertNotEquals("Should be 100", 10, testObj.getTotalPositions());
     }
 
-     @Test
-    public void CheckSetCharacterPosition() {
+    @Test
+    public void setCharacterPosition() {
         GameController testObj = new GameController();
-        testObj.startGame();
-       // testObj.setCharacterPosition(0,0);
-     //  testObj.setCharacterPosition( new Point (0,0) );
-     //  assertNotNull(testObj.status.currentPosition);
-      //  testObj.setCharacterPosition(new Point (10,10) );
-      boolean validPos;
-      validPos =  testObj.gameMap.isPositionValid(new Point (0, 0) );
-        assertTrue(validPos);
+        Point point = new Point(5, 5);
+        testObj.setCharacterPosition(point);
+        assertNotNull("Valid Status", testObj.status);
+        assertNotNull("Valid Position", testObj.status.currentPosition);
+        assertEquals("Wrong Status current position", new Point(5, 5), testObj.status.currentPosition);
     }
 
 }
