a821110dd8a062fd29856c6f8440481af8e25a87
root <gvclavanya1@yahoo.com>
2023-08-18 14:17:44 +0000

    t4

--- src/test/java/com/levelup/forestsandmonsters/CharacterTest.java
+++ src/test/java/com/levelup/forestsandmonsters/CharacterTest.java
@@ -1,5 +1,32 @@
 package com.levelup.forestsandmonsters;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertNotNull;
+//import com.levelup.forestsandmonsters.Character;
+//import com.levelup.forestsandmonsters.GameController.DIRECTION;
+
+import org.junit.Test;
+
 public class CharacterTest {
-    
+
+    String characterName;
+  @Test
+    public void getname() {
+
+        Character chars=new Character();
+       chars.getname();
+        assertEquals("joe", chars.getname());
+       
+       //this.characterName = characterName
+    }
+ public void startingposition(){
+ Character chars=new Character();
+ chars.getPosition();
+ 
+ }
+public void aftermove(){
+ Character chars=new Character();
+
+ chars.move(null);
+}
 }
